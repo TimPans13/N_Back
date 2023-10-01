f82c79c030b08182782c2f93a4c39079c0bfa2b3
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-10-01 18:05:53 +0300

    fix pressed
    fix button pressed

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -9,13 +9,13 @@ public class ButtonScript : MonoBehaviour
     private int buttonID;
     public int ButtonID { get => buttonID; set => buttonID = value; }
     private GameManager gameManager;
-    private Image buttonImage;
+    [SerializeField]private Image buttonImage;
     public Color flashColor = Color.blue; 
 
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-        buttonImage = GetComponent<Image>();
+        ///buttonImage = GetComponent<Image>();
 
     }
     public void ButtonClick()
@@ -30,8 +30,9 @@ public class ButtonScript : MonoBehaviour
 
     private IEnumerator FlashButtonCoroutine()
     {
-        buttonImage.color = flashColor;
+        Color originalColor = buttonImage.color; // сохраняем оригинальный цвет кнопки
+        buttonImage.color = new Color(flashColor.r, flashColor.g, flashColor.b, 1.0f); // устанавливаем непрозрачный flashColor
         yield return new WaitForSeconds(gameManager.buttonFlashDuration);
-        buttonImage.color = Color.white; 
+        buttonImage.color = originalColor; // восстанавливаем оригинальный цвет
     }
 }
