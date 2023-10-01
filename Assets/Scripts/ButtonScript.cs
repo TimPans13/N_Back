7e860b225dd3ee51d6f83a48833b22e1ec3b53fc
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-10-01 17:14:05 +0300

    add gameplay
    add simple game play(i need to fix it)

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,15 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ButtonScript : MonoBehaviour
 {
-    private int buttonID;//private
-
+    private int buttonID;
     public int ButtonID { get => buttonID; set => buttonID = value; }
+    private GameManager gameManager;
+    private Image buttonImage;
+    public Color flashColor = Color.blue; 
 
-    //public void SetButtonID(int id)
-    //{
-    //    buttonID = id;
-    //}
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        buttonImage = GetComponent<Image>();
+
+    }
+    public void ButtonClick()
+    {
+        gameManager.OnButtonClicked(buttonID);
+    }
+
+    public void FlashButton()
+    {
+        StartCoroutine(FlashButtonCoroutine());
+    }
+
+    private IEnumerator FlashButtonCoroutine()
+    {
+        buttonImage.color = flashColor;
+        yield return new WaitForSeconds(gameManager.buttonFlashDuration);
+        buttonImage.color = Color.white; 
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] private Transform container;
     private int gridSize = 9;
+    private List<ButtonScript> buttonCollection = new List<ButtonScript>();
+    private List<int> targetCombination = new List<int>();
+    private List<int> playerCombination = new List<int>();
+    private int currentCombinationSize = 1;
+    public float buttonFlashDuration = 1f; 
+    private float flashTimer = 0f;
 
     private void Start()
     {
         SpawnButtons();
+        GenerateTargetCombination();
+        StartCoroutine(FlashButtonsInCombination());
     }
 
     void SpawnButtons()
@@ -18,6 +28,74 @@ public class GameManager : MonoBehaviour
             GameObject card = Instantiate(buttonPrefab, container);
             ButtonScript buttonScript = card.GetComponent<ButtonScript>();
             buttonScript.ButtonID = i;
+            buttonCollection.Add(buttonScript);
         }
     }
+
+    void GenerateTargetCombination()
+    {
+        targetCombination.Clear();
+        for (int i = 0; i < currentCombinationSize; i++)
+        {
+            int randomButtonID = Random.Range(0, gridSize);
+            targetCombination.Add(randomButtonID);
+        }
+    }
+
+    IEnumerator FlashButtonsInCombination()
+    {
+        while (true)
+        {
+            flashTimer += Time.deltaTime;
+            if (flashTimer >= buttonFlashDuration)
+            {
+                flashTimer = 0f;
+                yield return new WaitForSeconds(1f); 
+                foreach (int buttonID in targetCombination)
+                {
+                    buttonCollection[buttonID].FlashButton(); 
+                    yield return new WaitForSeconds(buttonFlashDuration);
+                }
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    public void OnButtonClicked(int buttonID)
+    {
+        playerCombination.Add(buttonID);
+        if (playerCombination.Count == targetCombination.Count)
+        {
+            bool isCorrect = true;
+            for (int i = 0; i < targetCombination.Count; i++)
+            {
+                if (targetCombination[i] != playerCombination[i])
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+
+            if (isCorrect)
+            {
+                currentCombinationSize++;
+                GenerateTargetCombination();
+                flashTimer = 0f; 
+                StartCoroutine(FlashButtonsInCombination()); 
+                Debug.Log("ты чертовски прав");
+            }
+            else
+            {
+                currentCombinationSize = Mathf.Max(1, currentCombinationSize - 1);
+                GenerateTargetCombination();
+                flashTimer = 0f; 
+                StartCoroutine(FlashButtonsInCombination()); 
+                Debug.Log("не твой день братиш");
+            }
+
+            playerCombination.Clear();
+        }
+    }
+
 }
